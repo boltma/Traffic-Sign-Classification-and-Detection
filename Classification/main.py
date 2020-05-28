0c1d5c6539242f1c954108a4f37840b858b22c21
TTyRAeLL <liusheng17@mails.tsinghua.edu.cn>
2020-05-28 17:30:11 +0800

    add args

--- Classification/main.py
+++ Classification/main.py
@@ -10,13 +10,16 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Classification')
     parser.add_argument('--cuda', default='True')
     parser.add_argument('--epoch', type=int, default=100)
-    parser.add_argument('--model', default='ResNet18')
+    parser.add_argument('--model', default='ResNet18', help = 'You can choose: ResNet18, ResNet50, Inception, DenseNet')
+    parser.add_argument('--lr', type = float, default=0.001)
+    parser.add_argument('--BS', type = int, default=16, help = 'BatchSize')
+    parser.add_argument('--load', default='False', help='if you load an existing model or not')
     return parser.parse_args()
 def main():
     args = parse_args()
     save = True
     use_gpu = args.cuda == 'True'
-
+    load = args.load == 'True'
     train_tfs = transforms.Compose([
         transforms.Resize(299),
         transforms.RandomSizedCrop(299),
@@ -28,8 +31,8 @@ def main():
     dataset_size = ds.__len__()
     print(dataset_size)
     train_ds, val_ds = torch.utils.data.random_split(ds, [13000, 1463])
-    train_loader = torch.utils.data.DataLoader(train_ds, 16, False, num_workers = 8)
-    val_loader = torch.utils.data.DataLoader(val_ds, 16, False, num_workers = 8)
+    train_loader = torch.utils.data.DataLoader(train_ds, args.BS, False, num_workers = 8)
+    val_loader = torch.utils.data.DataLoader(val_ds, args.BS, False, num_workers = 8)
     print('train: ', len(train_ds))
     print('validation:', len(val_ds))
     print(type(ds), type(train_ds))
@@ -43,8 +46,9 @@ def main():
         test_model = model.DenseNet()
     if use_gpu:
         test_model = test_model.cuda()
-    #test_model.load_state_dict(torch.load('paramsdnn.pkl'))
-    optimizer = optim.Adam(test_model.parameters(), lr = 0.001)
+    if load:
+        test_model.load_state_dict(torch.load('params' + args.model + '.pkl'))
+    optimizer = optim.Adam(test_model.parameters(), lr = args.lr)
     print(use_gpu)
     result = train(test_model, args.epoch, optimizer, train_loader, val_loader, args.model, save, use_gpu)
     test(result, val_loader, use_gpu)
